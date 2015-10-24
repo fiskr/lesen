62e179e0152f89dca7ec5dc06a6bb6561e5acf36
Brandon Foster <bfoster@scrippsnetworks.com>
2015-10-24 13:07:15 -0400

    standard input is now treated as lines from a lesen source file

--- to-code.py
+++ to-code.py
@@ -20,6 +20,8 @@ else:
 
 if args.filename is not None:
     fileName = args.filename
+else:
+    fileName = None
 
 # Global vars
 CodeBlockName = ''
@@ -30,57 +32,69 @@ Mode = 'document'
 
 codeChunks = {}
 
-if fileName is not None:
-    file = open(fileName)
+# create dictionary made up of code chunks processed from each line of input
+def generateCodeChunks(line):
+    # print 'DEBUG: generateCodeChunks: Line: ' + line
+    global CodeBlockName
+    global Mode
+    global codeChunks
+    if (line[0] == '@'):
+        # print 'DEBUG: Document Mode Engaged! (generateCodeChunks)'
+        Mode = 'document'
+    if (line[0] == '<') and (line[1] == '<'):
+        previousCodeBlockName = CodeBlockName # backup the code block name
+        CodeBlockName = ""
 
-    for line in file: # run through the document once to setup the code chunks
-        if (line[0] == '@'):
-            Mode = 'document'
-        if (line[0] == '<') and (line[1] == '<'):
-            previousCodeBlockName = CodeBlockName # backup the code block name
-            CodeBlockName = ""
-
-            for i in range(2,len(line)): # parse out the name of the code chunk
-                if (line[i] == '>'): # check to see if it's the end of the name chunk
+        for i in range(2,len(line)): # parse out the name of the code chunk
+            if (line[i] == '>'): # check to see if it's the end of the name chunk
+                i += 1
+                if (line[i] == '>'): # it's either a reference or an addition to code chunk
                     i += 1
-                    if (line[i] == '>'): # it's either a reference or an addition to code chunk
-                        i += 1
-                        if (line[i] == '='): # it's an addition
-                            Mode = 'code'
-                            if CodeBlockName not in codeChunks:
-                                codeChunks[CodeBlockName] = []
-                            break
-                        else: # does not end in =, it's just a reference
-                            referenceCodeBlockName = CodeBlockName # save the reference code-block name as such
-                            CodeBlockName = previousCodeBlockName # set the code-block name back to what it was
-                            codeChunks[CodeBlockName].append(line)
-                            break
-                    else: # does not have second > of >>
-                        CodeBlockName += line[i]
+                    if (line[i] == '='): # it's an addition
+                        Mode = 'code'
+                        if CodeBlockName not in codeChunks:
+                            # print 'DEBUG: CodeBlockName: ' + CodeBlockName + ' is not in codeChunks: ' + str(codeChunks)
+                            codeChunks[CodeBlockName] = []
+                        break
+                    else: # does not end in =, it's just a reference
+                        referenceCodeBlockName = CodeBlockName # save the reference code-block name as such
+                        CodeBlockName = previousCodeBlockName # set the code-block name back to what it was
+                        codeChunks[CodeBlockName].append(line)
+                        break
                 else: # does not have second > of >>
                     CodeBlockName += line[i]
+            else: # does not have second > of >>
+                CodeBlockName += line[i]
+    else: # does not start with << or @
+        if (Mode == 'code') and (CodeBlockName is not None) and (CodeBlockName != ''):
+            # print 'DEBUG: appending to code chunk: ' + CodeBlockName
+            codeChunks[CodeBlockName].append(line)
 
-
-        else: # does not start with << or @
-            if (Mode == 'code') and (CodeBlockName is not None) and (CodeBlockName != ''):
-                codeChunks[CodeBlockName].append(line)
-
-    def processChunk(codeChunkName, prefixPadding=""):
-        for chunk in codeChunks[codeChunkName]:
-            codeChunkMatches = re.match('^.*<<([^>]+)>>.*$', chunk)
-            surroundingMatches = re.match('^(.*?)<<[^>]+>>(.*)$', chunk)
-            if surroundingMatches is not None:
-                referencePadding = surroundingMatches.groups()
-            if codeChunkMatches is not None:
-                codeChunkReferenceNames = codeChunkMatches.groups() # even though it's "names", you should expect only one group - use item [0]
-                if (codeChunkReferenceNames is not None) and (len(codeChunkReferenceNames) > 0):
-                    processChunk(codeChunkReferenceNames[0], prefixPadding + referencePadding[0])
-            else:
-                sys.stdout.write(prefixPadding + chunk)
-
-    processChunk(MasterBlockName)
-
+if fileName is not None:
+    file = open(fileName)
+    for line in file: # run through the document once to setup the code chunks
+        # print 'DEBUG: Line: ' + line
+        generateCodeChunks(line)
     file.close()
+else:
+    for line in sys.stdin:
+        generateCodeChunks(line)
 
+def processChunk(codeChunkName, prefixPadding=""):
+    global codeChunks
+    for chunk in codeChunks[codeChunkName]:
+        codeChunkMatches = re.match('^.*<<([^>]+)>>.*$', chunk)
+        surroundingMatches = re.match('^(.*?)<<[^>]+>>(.*)$', chunk)
+        if surroundingMatches is not None:
+            referencePadding = surroundingMatches.groups()
+        if codeChunkMatches is not None:
+            codeChunkReferenceNames = codeChunkMatches.groups() # even though it's "names", you should expect only one group - use item [0]
+            if (codeChunkReferenceNames is not None) and (len(codeChunkReferenceNames) > 0):
+                processChunk(codeChunkReferenceNames[0], prefixPadding + referencePadding[0])
+        else:
+            sys.stdout.write(prefixPadding + chunk)
+
+processChunk(MasterBlockName)
+#
 # codeChunks
 # pprint(codeChunks)
